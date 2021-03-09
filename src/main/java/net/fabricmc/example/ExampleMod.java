e922c117ee3c2a36c9d1b94d672e7c1a6141ea14
Anders Grasdal <WobblyMusicStand@gmail.com>
2021-03-08 20:05:51 -0800

    -Initial
    Signed-off-by: Anders Grasdal <WobblyMusicStand@gmail.com>

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,78 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.ObjectUtils;
+/*
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.util.math.BlockPos;
+import net.fabricmc.ex
+import net.minecraft.world.RaycastContext;
+ */
 
 public class ExampleMod implements ModInitializer {
+	private static MinecraftClient mcInstance;
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		/*Vec3d ray = new Vec3d;
+		mc.world.raycastBlock();
+		ProjectileUtil.raycast();
+		mc.world.playSound();
+		SoundMana*/
 		System.out.println("Hello Fabric world!");
+	}
+
+	public static void init(){
+		mcInstance = MinecraftClient.getInstance();
+		System.out.println("Initializing Sound Plugin...");
+	}
+
+	/*Test of simple audio "shading" modification. Calculates the # blocks between source and listener and decreases volume accordingly.*/
+	public static void occlusion(SoundInstance sound){
+	   /* PlayerEntity player = mcInstance.player;
+	    World world = mcInstance.world;
+        Vec3d playerPos;
+
+	    try {
+            playerPos = player.getPos();
+            Vec3d soundPos = new Vec3d(sound.getX(), sound.getY(), sound.getZ());
+            BlockHitResult bk = world.raycast(new RaycastContext(soundPos, playerPos, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.ANY, player));
+            HitResult.Type type = bk.getType();
+            //while ()
+            if (type != HitResult.Type.BLOCK) {
+                System.out.println("No blocks between " + sound.getId() + "and listener");
+            } else {
+                BlockState hit = world.getBlockState(bk.getBlockPos());
+                System.out.println("Hit block between sound source and player " +  hit.getSoundGroup() + " at " + bk.getBlockPos());
+            }
+            //BlockView.raycast();
+
+        } catch (NullPointerException e) {
+	        //yeet
+        }
+*/
+
+
 	}
 }
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,15 +1,54 @@
 package net.fabricmc.example.mixin;
 
+import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.sound.Sound;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.client.sound.TickableSoundInstance;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.sound.SoundCategory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+@Mixin(SoundManager.class)
+public class ExampleMixin{
+	@Inject(at = @At("HEAD"), method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", cancellable = true)
+	private void playOnce(SoundInstance sound, CallbackInfo info) {
+		//System.out.println("This line is printed each time a sound plays");
+		if (sound.getCategory() == SoundCategory.BLOCKS){
+			System.out.println("Currently Playing " + sound.getId() + " " + sound.getCategory() + " at " + sound.getX() + " " + sound.getY() + " " + sound.getZ());
+			ExampleMod.occlusion(sound);
+			//info.soundSystem.play(sound);
+		}
+		//info.cancel();
+
+	}
+
+
+	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/client/options/GameOptions;)V")
+	private void init(ResourceManager resourceManager, GameOptions gameOptions, CallbackInfo info) {
+		//System.out.println("This line is printed each time a sound plays");
+		System.out.println("Calling ExampleMod init from SoundManager init.");
+		ExampleMod.init();
+	}
+
+	@Inject(at = @At("HEAD"), method = "play(Lnet/minecraft/client/sound/SoundInstance;I)V")
+	private void playDelay(SoundInstance sound, int delay, CallbackInfo info) {
+		//System.out.println("This line is printed each time a sound plays");
+		System.out.println("Queuing "+sound.getId()+" with "+delay+" ms delay");
+
+	}
+
+	@Inject(at = @At("HEAD"), method = "playNextTick")
+	private void playTickable(TickableSoundInstance sound, CallbackInfo info) {
+		//System.out.println("This line is printed each time a sound plays");
+		System.out.println("Currently Playing Tickable: " + sound.getId());
+
 	}
 }
+
+
